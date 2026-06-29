468992b022a6b5ca924c59a90d97e1d2a732defc
JAI <92963661+jai333@users.noreply.github.com>
2026-06-30 01:48:14 +0530

    Update Gmail SMTP App Password

--- app_track/ats_crm_project/settings.py
+++ app_track/ats_crm_project/settings.py
@@ -176,7 +176,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jaisukhwal41@gmail.com'
-EMAIL_HOST_PASSWORD = 'midwab-hosgo5-qiwhaN' # App password
+EMAIL_HOST_PASSWORD = 'zgihpkfvtwliixrg' # App password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
